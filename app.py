ffa2c1e7e2edfa8d7f11aa576511093eaea7cf9a
hamza <hamzacodes97@gmail.com>
2023-05-25 11:11:10 +0000

    Jenkinsfile added

--- app.py
+++ app.py
@@ -16,4 +16,4 @@ def home():
     return f"<h1>Hello {name}.</h1>\n\n<h2>I'm currently running in {hostname}.</h2>\n"
 
 if __name__=='__main__':
-  app.run(host='0.0.0.0', port=5500, debug=True)
+  app.run(host='0.0.0.0', port=5000, debug=True)
